2e296113e9c64e0e946c6b73ec6e053b8e885517
Antonio Aleman <antonio.9714@gmail.com>
2020-05-20 22:23:54 -0500

    deleting unnecessary comments

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ from ShifterFactory import Creator, CircularShifterCreator
 from Input import Input
 
 def client_code(creator: Creator, entrada) -> None:
-    """
-    The client code works with an instance of a concrete creator, albeit through
-    its base interface. As long as the client keeps working with the creator via
-    the base interface, you can pass it any creator's subclass.
-    """
-
     print("Client: I'm not aware of the creator's class, but it still works.")
     print(creator.some_operation(entrada))
 
